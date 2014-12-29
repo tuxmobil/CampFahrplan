c49fec29b64922bd4f8f0c733012103ffcf82eb5
nohum <eiskalterengel@truthfactory.tk>
2014-12-29 13:06:37 +0100

    change logging conditions of LogUtil

--- wear/src/main/java/nerd/tuxmobil/fahrplan/congress/LogUtil.java
+++ wear/src/main/java/nerd/tuxmobil/fahrplan/congress/LogUtil.java
@@ -7,9 +7,9 @@ public class LogUtil {
     private static final String TAG = "CampFahrplan:Wear";
 
     public static void debug(String message) {
-//        if (Log.isLoggable(TAG, Log.DEBUG)) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
             Log.d(TAG, message);
-//        }
+        }
     }
 
     public static void info(String message) {
